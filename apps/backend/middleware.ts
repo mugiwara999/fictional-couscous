dc340c63510495d554f5e5a6aaf88955761b616b
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2025-05-20 11:06:52 +0530

    update clerk issuer

--- apps/backend/middleware.ts
+++ apps/backend/middleware.ts
@@ -45,7 +45,7 @@ export async function authMiddleware(
     const decoded = jwt.verify(token, formattedKey, {
       algorithms: ["RS256"],
       issuer:
-        process.env.CLERK_ISSUER || "https://clerk.100xdevs.com",
+        process.env.CLERK_ISSUER || "https://clerk.photo.rahulkoyye.top/",
       complete: true,
     });
 
